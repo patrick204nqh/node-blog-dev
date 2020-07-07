a6ca472c1800bb48a415d2b8e3d34d4fabb34808
patrick <patrick204nqh@gmail.com>
2020-07-07 14:19:05 +0700

    global object

--- global.js
+++ global.js
@@ -1,9 +1,14 @@
 // Global object
 
-setTimeout(() => {
-  console.log('in the timeout');
-}, 3000);
+// setTimeout(() => {
+//   console.log('in the timeout');
+// }, 3000);
 
-const int = setInterval(() => {
-  console.log('in the interval');
-}, 1000);
+// const int = setInterval(() => {
+//   console.log('in the interval');
+// }, 1000);
+
+// console.log(__dirname);
+// console.log(__filename);
+
+console.log(document.querySelector); // error with global object
