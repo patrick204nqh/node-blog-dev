7c831881ea09c146fa75aca6f4ecbe0493f9be56
patrick <patrick204nqh@gmail.com>
2020-07-08 12:01:08 +0700

    response object

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader('Content-Type', 'text/html');
+
+  res.write('<head><link rel="stylesheet" href="#"></head>');
+  res.write('<h1>Karl Patrick</h1>');
+  res.write('<p>hello, world!</p>');
+  res.end();
 });
 
 server.listen(3000, 'localhost', () => {
