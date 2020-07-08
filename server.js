df88afae9e277bbe17366d1cdedcb8bdb0aad979
patrick <patrick204nqh@gmail.com>
2020-07-08 23:29:52 +0700

    creating an express app

--- server.js
+++ server.js
@@ -3,17 +3,6 @@ const fs = require('fs');
 const _ = require('lodash');
 
 const server = http.createServer((req, res) => {
-  // lodash
-  const num = _.random(0, 20);
-  console.log(num);
-
-  const greet = _.once(() => {
-    console.log('hello');
-  });
-
-  greet();
-  greet();
-
   // set header content type
   res.setHeader('Content-Type', 'text/html');
 
